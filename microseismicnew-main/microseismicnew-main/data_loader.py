5af38bc28836fe690a63a54aaed2faee70594708
bright01-collab <ikponmwosaiyegbekedobright@gmail.com>
2024-06-28 16:05:17 +0100

    Update data_loader.py

--- microseismicnew-main/microseismicnew-main/data_loader.py
+++ microseismicnew-main/microseismicnew-main/data_loader.py
@@ -2,8 +2,8 @@ import pandas as pd
 
 def load_data(dataset, start_date, end_date):
     if dataset == "SLB Data":
-        data = pd.read_csv('microseismicnew-main/microseismicnew-main
-/miSLB_Data.csv')
+        data = pd.read_csv('./microseismicnew-main/microseismicnew-main
+/SLB_Data.csv')
         origin_time_col = 'SLB origin time'
     elif dataset == "Relocated Data":
         data = pd.read_csv('./microseismicnew-main/microseismicnew-main
